4af3394cf3a588f823698042ab53c09ba9c7a0a4
Jouni Airaksinen <jouni.airaksinen@dnainternet.net>
2018-06-07 11:33:31 +0300

    4.2 blogilista, osa 2

--- index.js
+++ index.js
@@ -3,41 +3,26 @@ const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
+const blogsRouter = require('./controllers/blogs')
 
-const Blog = mongoose.model('Blog', {
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 
-module.exports = Blog
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then( () => {
+    console.log('connected to database', process.env.MONGODB_URI)
+  })
+  .catch( err => {
+    console.log(err)
+  })
 
 app.use(cors())
 app.use(bodyParser.json())
+app.use('/api/blogs', blogsRouter)
 
-const mongoUrl = 'mongodb://jabloglist:jablogilista21@ds147420.mlab.com:47420/bloglist-ja'
-mongoose.connect(mongoUrl)
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = 3003
+const PORT = process.env.PORT || 3003
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
