69bda100998101acbd6b70034adfb28627518a42
Jouni Airaksinen <jouni.airaksinen@dnainternet.net>
2018-06-08 03:10:29 +0300

    4.7* apufunktioita ja yksikkötestejä, osa 5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -19,10 +19,22 @@ const mostBlogs = (blogs) => {
       blogs: blogs.filter(blog => blog.author === a).length
     }
   })
-
   return blogsByAuthor.reduce((most, item) => (most.blogs > item.blogs) ? most : item)
 }
 
+const mostLikes = (blogs) => {
+  const authors = [...new Set( blogs.map(blog => blog.author ) )]
+  const authorsLikes =  authors.map(a => {
+    let blogsBy = blogs.filter(blog => blog.author === a)
+    return {
+      author: a,
+      likes: blogsBy.map(blog => blog.likes).reduce((sum, item) => sum + item, 0)
+    }
+  })
+
+  return authorsLikes.reduce((most, item) => (most.likes > item.likes) ? most : item)
+}
+
 module.exports = {
-  dummy, totalLikes, favoriteBlog, mostBlogs
+  dummy, totalLikes, favoriteBlog, mostBlogs, mostLikes
 }
