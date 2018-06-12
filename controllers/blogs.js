e8a284f432b57f2447e5559ab7361b2222b7b61f
Jouni Airaksinen <jouni.airaksinen@dnainternet.net>
2018-06-12 20:37:20 +0300

    4.20* blogilistan laajennus, osa 9

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -3,13 +3,7 @@ const Blog = require('../models/blog')
 const User = require('../models/user')
 const jwt = require('jsonwebtoken')
 
-const getTokenFrom = (request) => {
-  const authorization = request.get('authorization')
-  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-    return authorization.substring(7)
-  }
-  return null
-}
+
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog
@@ -20,7 +14,9 @@ blogsRouter.get('/', async (request, response) => {
 
 blogsRouter.get('/:id', async (request, response) => {
   try {
-    const blog = await Blog.findById(request.params.id)
+    const blog = await Blog
+      .findById(request.params.id)
+      .populate('user', { username: 1, name: 1 } )
 
     if (blog) {
       return response.json(Blog.format(blog))
@@ -38,10 +34,10 @@ blogsRouter.post('/', async (request, response) => {
   const body = request.body
 
   try {
-    const token = getTokenFrom(request)
-    const decodedToken = jwt.verify(token, process.env.SECRET)
+    //const token = getTokenFrom(request)
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
 
-    if (!token || !decodedToken.id) {
+    if (!request.token || !decodedToken.id) {
       return response.status(401).json({ error: 'token missing or invalid' })
     }
 
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
+const middleware = require('./utils/middleware')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
@@ -20,9 +21,10 @@ mongoose
 
 app.use(cors())
 app.use(bodyParser.json())
-app.use('/api/blogs', blogsRouter)
-app.use('/api/users', usersRouter)
+app.use(middleware.tokenExtractor)
 app.use('/api/login', loginRouter)
+app.use('/api/users', usersRouter)
+app.use('/api/blogs', blogsRouter)
 
 const server = http.createServer(app)
 
